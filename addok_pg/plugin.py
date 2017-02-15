6132a59cccaa3dbc40371f6621be941806a889d7
cquest <cquest@openstreetmap.fr>
2017-02-15 18:25:13 +0100

    use psycopg2.pool + update for addok 1.0.0rc1

--- addok_pg/plugin.py
+++ addok_pg/plugin.py
@@ -1,16 +1,24 @@
 import psycopg2
+import psycopg2.pool
 import os
 
 from addok.config import config
 
 
 class PGStore:
-
     def __init__(self, *args, **kwargs):
-        with PGStore.conn() as conn:
+        self.pg = psycopg2.pool.PersistentConnectionPool(8,64,config.PG_CONFIG)
+        with self.pg.getconn() as conn:
             cur = conn.cursor()
             cur.execute('CREATE TABLE IF NOT EXISTS '+config.PG_TABLE+' (key VARCHAR COLLATE "C", data bytea)')
             cur.execute('CREATE UNIQUE INDEX IF NOT EXISTS '+config.PG_TABLE+'_key_idx ON '+config.PG_TABLE+' (key)')
+            conn.commit()
+            self.pg.putconn(conn)
+
+    def flushdb(self):
+        with PGStore.conn() as conn:
+            cur = conn.cursor()
+            cur.execute('DROP TABLE IF EXISTS '+config.PG_TABLE)
             conn.commit()
 
     def conn():
@@ -20,14 +28,17 @@ class PGStore:
         if not keys:  # Avoid invalid SQL.
             return
         keys = [key.decode() for key in keys]
-        with PGStore.conn().cursor() as cur:
-            params = ','.join("'%s'" % k for k in keys)
+        with self.pg.getconn() as conn:
+            cur = conn.cursor()
+            params = ','.join(cur.mogrify("'%s'" % k).decode("utf-8") for k in keys)
             query = 'SELECT key, data FROM '+config.PG_TABLE+' WHERE key IN ('+params+')'
+            print(query)
             cur.execute(query)
             for key, data in cur.fetchall():
                 yield key.encode(), data
+            self.pg.putconn(conn)
 
-    def add(self, *docs):
+    def upsert(self, *docs):
         with PGStore.conn() as conn:
             cur = conn.cursor()
             values = ','.join(cur.mogrify("(%s,%s)", d).decode("utf-8") for d in docs)
@@ -41,6 +52,6 @@ class PGStore:
 
 
 def preconfigure(config):
-    config.DOCUMENT_STORE = 'addok_pg.plugin.PGStore'
+    config.DOCUMENT_STORE_PYPATH = 'addok_pg.plugin.PGStore'
     config.PG_CONFIG = 'dbname=addok user=addok host=localhost password=addok'
     config.PG_TABLE = 'addok'
